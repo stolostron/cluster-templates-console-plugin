665dbf34181e16e149a0656a03fd9e38fa7f8282
Rastislav Wagner <rawagner@redhat.com>
2023-01-17 12:12:44 +0100

    Remove helm chart and deployment scripts. The UI is now being deployed by CLaaS operator.

--- scripts/patch-deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-oc scale deployment.v1.apps/caas-ui -n caas-ui --replicas=0
-
-sed 's/__USER__/'"$USER"'/g' ./scripts/deploy-ui.yaml > /tmp/out.yaml
-oc apply -f /tmp/out.yaml
-rm -f /tmp/out.yaml
-
-oc scale deployment.v1.apps/caas-ui -n caas-ui --replicas=2
