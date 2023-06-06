a0353737205d4ae44616ef5cd2ee19df1d7ea052
Bat-Zion Rotman <brotman@redhat.com>
2023-06-06 16:52:34 +0300

    fix deployment script to use config instead of configmap (#44)

--- scripts/deploy/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "Error: no image provided. Run ${0} <<image>>"
-    exit 1
-fi
-DIR=$(dirname $0)
-oc process -p IMAGE=$1 -f ${DIR}/class-config-map-template.yaml | oc apply -f -
